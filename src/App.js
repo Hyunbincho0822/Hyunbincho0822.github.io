92eb9b7663599c4dccde21af78950a85b09056ea
Hyunbincho0822 <102786139+Hyunbincho0822@users.noreply.github.com>
2022-12-29 16:18:27 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
 
-const App = () => {
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <h1>Jinseok's Betting Game</h1>
+      <a href="https://replit.com:/@hyunbincho0822/Python-1?s=app">입장하시겠습니까?</a>
     </div>
-  )
+  );
 }
-
-export default App
